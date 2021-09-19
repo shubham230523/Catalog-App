36a0daa8bde98467ea37384ba8cd17878ed0a29f
Shubham Thorat <shubhamthorat230520011@gmail.com>
2021-09-19 18:11:19 +0530

    catalog.json

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/utils/widgets/drawer.dart';
 import 'package:flutter_catalog/utils/widgets/item_widgets.dart';
-class homepage extends StatelessWidget {
+import 'dart:convert';
+class homepage extends StatefulWidget {
 
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
 final dummyList= List.generate(20, (index) => CatalogModels.items[0]);
+
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  loadData() async {
+    final catalogJson  = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData['products'];
+    
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,3 +47,42 @@ final dummyList= List.generate(20, (index) => CatalogModels.items[0]);
       ); 
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//google pixel 5a
+//https://m.media-amazon.com/images/I/71C0OH4WfpL._SX679_.jpg
+
+//macbook air
+//https://www.climaxcomputer.com/wp-content/uploads/2020/05/New-Apple-Macbook-Air-Gold-New.jpg
+
+//play station5 
+//https://rukminim1.flixcart.com/image/416/416/kj7gwi80/gamingconsole/n/3/c/cfi-1008b01r-825-sony-no-original-imafytxenahqnnpu.jpeg?q=70
+
+//airpods pro
+//https://media.croma.com/image/upload/f_auto,q_auto,d_Croma%20Assets:no-product-image.jpg,h_350,w_350/v1605170750/Croma%20Assets/Entertainment/Headphones%20and%20Earphones/Images/8937376841758.png
+
+//galaxy s21 ultra
+//https://cdn.shopify.com/s/files/1/0245/9382/5867/products/91BAitkzszL._SL1500_600x.jpg?v=1617105721
+
+
+//one plus 9 5g
+//https://images-eu.ssl-images-amazon.com/images/I/417uExCG6KL._SX300_SY300_QL70_FMwebp_.jpg
