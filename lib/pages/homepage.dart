85f0698b7fc64f3bf57579105760a8b8a9553023
Shubham Thorat <shubhamthorat230520011@gmail.com>
2021-09-19 19:15:02 +0530

    gridView partially learned

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -39,10 +39,45 @@ class _homepageState extends State<homepage> {
       ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: (CatalogModels.items!=null && CatalogModels.items.isNotEmpty)? ListView.builder(
+          child: (CatalogModels.items!=null && CatalogModels.items.isNotEmpty)? 
+          GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              ),
             itemCount: CatalogModels.items.length,
             itemBuilder: (context , index){
-              return ItemWidget(item: CatalogModels.items[index]);
+              final item = CatalogModels.items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                child: GridTile(
+                  header: Container(
+                    child: Text(item.name ,
+                     style: TextStyle(
+                       color: Colors.white,
+                    ),),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+
+                    ),
+                    ),
+                  child: Image.network(item.image),
+                  footer:  Container(
+                    child: Text(item.price.toString() ,
+                     style: TextStyle(
+                       color: Colors.white,
+                    ),),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+
+                    ),
+                    ),
+                  )
+                );
             },
           ):Center(
             child: CircularProgressIndicator(),
